10307952034dc9600a8178c2edf91f8bfc50d38d
grafe <maximizhevsk@gmail.com>
2025-03-12 11:27:17 +0400

    homework interfaces done

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,5 +1,8 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.article.Article;
+import org.skypro.skyshop.search.SearchEngine;
+
 import java.util.Arrays;
 
 public class App {
@@ -29,7 +32,7 @@ public class App {
         int totalCost = basket.getTotalCost();  //получение общей стоимости корзины
         System.out.printf("Общая стоимость корзины: %d\n", totalCost);
 
-        basket.hasProduct();  //поиск продукта в корзине
+        //basket.hasProduct();  //поиск продукта в корзине
 
         basket.clearBasket();  //очистка корзины
         basket.printBasketContent();  //печать содержимого корзины
@@ -37,7 +40,42 @@ public class App {
         totalCost = basket.getTotalCost();  //получение общей стоимости
         System.out.printf("Общая стоимость корзины: %d\n", totalCost);
 
-        basket.hasProduct();  //поиск продукта в корзине
+        //basket.hasProduct();  //поиск продукта в корзине
+
+        //Создайте один объект типа
+        //SearchEngine
+        // и добавьте в него все товары, которые создаются для проверки других методов.
+
+        SearchEngine searchEngine = new SearchEngine();
+        searchEngine.add(apple);
+        searchEngine.add(banana);
+        searchEngine.add(orange);
+        searchEngine.add(pear);
+        searchEngine.add(grape);
+        searchEngine.add(peach);
+
+        //Создайте несколько объектов типа
+        //Article
+        // и добавьте их в
+        //Search Engine
+
+        Article table = new Article("Стол", "Стол кухонный раздвижной обеденный.");
+        Article chair = new Article("Стул", "Коричневый деревянный стул идеально впишется.");
+
+        searchEngine.add(table);
+        searchEngine.add(chair);
+
+        String searchQuery = "Банан";
+        System.out.println("Поиск /" +  searchQuery + "/ " + Arrays.toString(searchEngine.search(searchQuery)));
+
+        String searchQuery1 = "Апельсин";
+        System.out.println("Поиск /" +  searchQuery1 + "/ " + Arrays.toString(searchEngine.search(searchQuery1)));
+
+        String searchQuery2 = "Манго";
+        System.out.println("Поиск /" +  searchQuery2 + " /" + Arrays.toString(searchEngine.search(searchQuery2)));
+
+
+
 
 
     }
